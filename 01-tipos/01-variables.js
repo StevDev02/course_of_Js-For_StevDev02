255d4a43fce14b42a4d9fb8dc91a626c95daccf1
StevDev <stevdev04@gmail.com>
2024-04-20 20:34:56 -0500

    Master: 01- Tipos de datos o variables

---
+++ 01-tipos/01-variables.js
@@ -0,0 +1,15 @@
+
+//Todo: Variables
+//?: camelCase
+
+//Diferencia
+let name = 'steven'
+
+const nameUser = 'Luis'
+
+//Let puede ser modificado
+name = undefined
+//const no puede ser modfiicado
+nameUser = 0
+
+
---
+++ 01-tipos/02-primitivos.js
@@ -0,0 +1,12 @@
+
+let number = 1
+let string = 'texto'
+let boleano = true
+let noDefino = undefined
+let nulo = null
+
+console.log(typeof number)
+console.log(typeof string)
+console.log(typeof boleano)
+console.log(typeof noDefino)
+console.log(typeof nulo)
