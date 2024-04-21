99ad1ac2b066e90898f689aafd70d905533b276e
StevDev <stevdev04@gmail.com>
2024-04-20 20:45:31 -0500

    04-tipado-dinamico

---
+++ 01-tipos/04-tipado-dinamico.js
@@ -0,0 +1,13 @@
+
+
+let number = 1
+let string = 'texto'
+let boleano = true
+let noDefino = undefined
+let nulo = null
+
+console.log(typeof number)
+console.log(typeof string)
+console.log(typeof boleano)
+console.log(typeof noDefino)
+console.log(typeof nulo)
